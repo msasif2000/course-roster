2e07cead6520a726787c0bf7688f0dd1a06b5100
Asif <msasifiiuc@gmail.com>
2023-09-15 06:52:08 +0600

    course json data created

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,19 @@ function App() {
 
   return (
     <>
-      <h1>Vite + React</h1>
+      <section className='bg-[#F3F3F3]'>
+        <div className='md:container mx-auto'>
+          <h1 className='text-3xl text-center py-6'>Course Registration</h1>
+          <div className='flex gap-4 p-4'>
+            <div className='w-3/4'>
+              <h2>Course</h2>
+            </div>
+            <div className='w-1/4'>
+              <h2>Cart</h2>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   )
 }
